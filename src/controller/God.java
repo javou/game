3d27442762539bbd6029b1d871440552617221bb
Artur <arturpaulos22@gmail.com>
2022-06-07 16:50:01 -0300

    pull adjustments

--- src/controller/God.java
+++ src/controller/God.java
@@ -18,8 +18,8 @@ public class God {
 		//create hero, castle, monster....
 		castle = new Castle();
 		
-		Timer timer = new Timer(10, new NextAction());
-		timer.start();
+		//Timer timer = new Timer(10, new NextAction());
+		//timer.start();
 		
 	}
 	
